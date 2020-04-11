c30c291dc1777cd2c059d0057ee371d526d32af5
Amarto Rajaram <amarto.rajaram@gmail.com>
2020-04-11 18:50:00 -0400

    Add better filtering for search results

--- cl_fitness_equip_scraper.py
+++ cl_fitness_equip_scraper.py
@@ -18,19 +18,21 @@ for item in ITEMS_OF_INTEREST:
 
     posts = html_soup.find_all('li', class_='result-row')
 
-    print(posts[0].find('span', class_='result-hood'))
     for post in posts:
         post_title = post.find('a', class_= 'result-title hdrlnk').text
-        days_since_posted = utils.get_days_since(post.find('time', class_= 'result-date')['datetime'])
-        post_price = utils.parse_price(post.find('span', class_= 'result-price').text)
-        post_hood = utils.clean_neighborhood(post.find('span', class_= 'result-hood'))
+        if utils.is_post_relevant(post_title, item):
+            post_price = utils.parse_price(post.find('span', class_= 'result-price').text)
+            days_since_posted = utils.get_days_since(post.find('time', class_= 'result-date')['datetime'])
+            post_price = utils.parse_price(post.find('span', class_= 'result-price').text)
+            post_hood = utils.clean_neighborhood(post.find('span', class_= 'result-hood'))
 
-        interesting_items[item]['results'].append({
-            'title': post_title,
-            'days_since_posted': days_since_posted,
-            'price': '$' + str(post_price),
-            'neighborhood': post_hood,
-        })
+            interesting_items[item]['results'].append({
+                'title': post_title,
+                'days_since_posted': days_since_posted,
+                'price': '$' + str(post_price),
+                'neighborhood': post_hood,
+            })
 
-    print(interesting_items)
+print(interesting_items)
+
 
--- utils.py
+++ utils.py
@@ -11,3 +11,15 @@ def clean_neighborhood(hood):
     if not hood:
         return None
     return hood.text.replace('(', '').replace(')', '')
+
+def is_post_relevant(post_title, search_str):
+    search_terms = search_str.split(' ')
+    search_term_hit_count = 0
+    post_title_terms = post_title.split(' ') # TODO probably make a separate util for this
+    post_title_terms = [term.lower() for term in post_title_terms]
+    post_title = ''.join(post_title_terms)
+    for term in search_terms:
+        if post_title.find(term) != -1:
+            search_term_hit_count += 1
+
+    return (search_term_hit_count/len(search_terms)) >= .5
